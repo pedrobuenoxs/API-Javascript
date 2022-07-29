2f40a3b97f84e95e2d8f503ffb96cd839847fa9b
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-29 00:15:23 -0300

    add jest and husky

---
+++ src/presentation/routers/login.router.spec.js
@@ -0,0 +1,22 @@
+class LoginRouter {
+  route(httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+      };
+    }
+  }
+}
+
+describe("Login Router", () => {
+  test("Should return 400 if no email is provided  ", () => {
+    const router = new LoginRouter();
+    const httpRequest = {
+      body: {
+        password: "123456",
+      },
+    };
+    const httpResponse = router.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
