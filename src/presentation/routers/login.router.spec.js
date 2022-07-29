7befc9aa9f327fdd48791e67c3bc344579641d1c
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-29 00:49:24 -0300

    add Http Response Class

--- src/presentation/routers/login.router.spec.js
+++ src/presentation/routers/login.router.spec.js
@@ -1,16 +1,36 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest) {
-      return {
-        statusCode: 500,
-      };
-    }
+    if (!httpRequest) return HttpResponse.serverError();
+    if (!httpRequest.body) return HttpResponse.serverError();
     const { email, password } = httpRequest.body;
-    if (!email || !password) {
-      return {
-        statusCode: 400,
-      };
-    }
+    if (!email) return HttpResponse.badRequest("email");
+    if (!password) return HttpResponse.badRequest("password");
+  }
+}
+
+class MissingParamsError extends Error {
+  constructor(param) {
+    super(`"Missing params: ${param}"`);
+    this.name = "MissingParamsError";
+  }
+}
+
+class HttpResponse {
+  static badRequest(paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamsError(paramName),
+    };
+  }
+  static ok() {
+    return {
+      statusCode: 200,
+    };
+  }
+  static serverError() {
+    return {
+      statusCode: 500,
+    };
   }
 }
 
@@ -24,6 +44,8 @@ describe("Login Router", () => {
     };
     const httpResponse = router.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body.name).toBe("MissingParamsError");
+    expect(httpResponse.body).toEqual(new MissingParamsError("email"));
   });
   test("Should return 400 if no password is provided  ", () => {
     const router = new LoginRouter();
@@ -34,11 +56,20 @@ describe("Login Router", () => {
     };
     const httpResponse = router.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body.name).toBe("MissingParamsError");
+    expect(httpResponse.body).toEqual(new MissingParamsError("password"));
   });
 
   test("Should return 500 if no httpRequest is provided  ", () => {
     const router = new LoginRouter();
     const httpResponse = router.route();
+    expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test("Should return 500 if httpRequest is null  ", () => {
+    const router = new LoginRouter();
+    const httpRequest = {};
+    const httpResponse = router.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
   });
 });
